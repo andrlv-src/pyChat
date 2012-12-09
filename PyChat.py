b7c8f6fd5bbed8b0344197b759f6d1223ff9dbd0
WoenZu <0x81zu@gmail.ru>
2012-12-09 19:08:59 +0400

    splitted threads

--- PyChat.py
+++ PyChat.py
@@ -1,29 +1,38 @@
 # -*- coding: utf-8 -*-
 
+
+
+## TODO разобраться с относительностью путей к смайлам после преобразования py2exe 
+##      иное решение - настройка пути к файлам смайлов в самом чате
+
 from PyQt4 import QtGui,  QtCore
 import sys
-import PyChatAPI.defaultlistener
+import PyChatAPI.defaultmsgprocessor
+import PyChatAPI.connector
 import PyChatGUI.pychatgui
-import PyChatAPI.defaultsender
 
 class PyChat(object):
     def __init__(self):
         app = QtGui.QApplication(sys.argv)
         self.gui = PyChatGUI.pychatgui.PyChatGui()
-        #self.listener = PyChatAPI.defaultlistener.DefaultListener()
+        self.ml = MainLogic()
+        self.ml.start()  
         self.gui.show()
         sys.exit(app.exec_())
         
 class MainLogic(QtCore.QThread):
-    # конструктор
-    # выполняем конструктор предка
-    # создаем объекты листенер и сендер 
     
-    
-    # переопределяем метод run()
-        # 
-    pass
-
+    def __init__(self, parent = None):
+        QtCore.QThread.__init__(self, parent)
+        self.connector = PyChatAPI.connector.Connector()
+            
+    def run(self):
+        
+        while True:
+            print self.connector.reciever()
+            # process message
+            # listen GUI
+            # process message
+        
 if __name__ == '__main__':
     PyChat()
-        
