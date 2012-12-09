58da1912bfd6081b47089d4e42d0e1130ee164e2
WoenZu <0x81zu@gmail.ru>
2012-12-09 19:04:29 +0400

    edited connector.py

--- PyChatAPI/connector.py
+++ PyChatAPI/connector.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 # connector class
-# 05/12/2012
+# 05.12.2012
 
 import socket
 
-# создаем сокет
+class Connector(object):
+	def __init__(self):
+		
+		# создаем сокет
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 
 		# подсоединяемся к чат-серверу
 		self.connect()
@@ -18,12 +20,20 @@ import socket
 		HOST = 'localhost'
 		# HOST = '10.1.9.93'	# адрес сервеоа
 		PORT = 6666			# порт вервера
-		print self.PROMPT + 'connecting to %s...' % HOST
+		print 'connecting to %s...' % HOST
 
 		try:
 			self.sock.connect((HOST, PORT))
-			print self.PROMPT + 'connected at', HOST,':', PORT
+			print 'connected at', HOST,':', PORT
 		except Exception, e:
-			print self.PROMPT + 'unable to connect at', HOST,':', PORT
+			print 'unable to connect at', HOST,':', PORT
 			raw_input('press enter')
 			exit(0)
+	def reciever(self):
+		return self.sock.recv(1024)
+
+if __name__ == '__main__':
+	c = Connector()
+	a = True
+	while a:
+		print c.reciever()
